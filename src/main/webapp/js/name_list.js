419ff23048d4401dd6cb62a9b86934a1665f6552
KoleHicok <kolehicok@gmail.com>
2021-02-18 08:34:16 -0600

    Basic data scrubbing

--- src/main/webapp/js/name_list.js
+++ src/main/webapp/js/name_list.js
@@ -1,5 +1,9 @@
 // Main Javascript File
 
+function htmlSafe(data){
+    return data.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt");
+}
+
 function updateTable(){
     let url = "api/name_list_get";
 
@@ -8,9 +12,10 @@ function updateTable(){
                 "<td>Last Name</td><td>Email</td><td>Phone</td><td>Birthday</td></tr>");
 
         for(let i=0; i < json_result.length; i++){
-            $('#datatable tbody tr:last').after(`<tr><td>${json_result[i].id}</td><td>${json_result[i].first}</td>` +
-                `<td>${json_result[i].last}</td><td>${json_result[i].email}</td><td>${json_result[i].phone}</td>` +
-                `<td>${json_result[i].birthday}</td></tr>`);
+            $('#datatable tbody tr:last').after(`<tr><td>${htmlSafe(json_result[i].id)}</td><td>` +
+                `${htmlSafe(json_result[i].first)}</td><td>${htmlSafe(json_result[i].last)}</td><td>` +
+                `${htmlSafe(json_result[i].email)}</td><td>${htmlSafe(json_result[i].phone)}</td><td>` +
+                `${htmlSafe(json_result[i].birthday)}</td></tr>`);
         }
 
         $('#datatable tbody tr:first').remove();
