9d183f23a851806e63649cc129fa6e3777fc535a
KoleHicok <kolehicok@gmail.com>
2021-02-25 10:02:19 -0600

    Added form and basic regex checks

--- src/main/webapp/js/name_list.js
+++ src/main/webapp/js/name_list.js
@@ -52,4 +52,112 @@ function updateTable(){
     });
 }
 
+function showDialogAdd(){
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#id').removeClass("is-invalid");
+    $('#id').removeClass("is-valid");
+
+    $('#firstName').val("");
+    $('#firstName').removeClass("is-invalid");
+    $('#firstName').removeClass("is-valid");
+
+    $('#lastName').val("");
+    $('#lastName').removeClass("is-invalid");
+    $('#lastName').removeClass("is-valid");
+
+    $('#email').val("");
+    $('#email').removeClass("is-invalid");
+    $('#email').removeClass("is-valid");
+
+    $('#phone').val("");
+    $('#phone').removeClass("is-invalid");
+    $('#phone').removeClass("is-valid");
+
+    $('#birthday').val("");
+    $('#birthday').removeClass("is-invalid");
+    $('#birthday').removeClass("is-valid");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+function saveChanges() {
+    console.log("Saving changes!");
+    let firstName = $('#firstName');
+    let firstreg = /^[A-Za-z]'?[-A-Za-zÁÉÍÓÚáéíóúñ]{1,15}$/;
+
+    if(firstreg.test(firstName.val())){
+        firstName.removeClass("is-invalid");
+        firstName.addClass("is-valid");
+    }
+    else{
+        firstName.removeClass("is-valid");
+        firstName.addClass("is-invalid");
+    }
+
+    let lastName = $('#lastName');
+    let reglast = /^[A-Za-z][-'A-Za-zÁÉÍÓÚáéíóúñ]{1,20}$/;
+
+    if(reglast.test(lastName.val())){
+        lastName.removeClass("is-invalid");
+        lastName.addClass("is-valid");
+    }
+    else{
+        lastName.removeClass("is-valid");
+        lastName.addClass("is-invalid");
+    }
+
+    let email = $('#email');
+    let regemail = /^.*@.*$/;
+
+    if(regemail.test(email.val())){
+        email.removeClass("is-invalid");
+        email.addClass("is-valid");
+    }
+    else{
+        email.removeClass("is-valid");
+        email.addClass("is-invalid");
+    }
+
+    let phone = $('#phone');
+    let regphone = /^\+?1?\s?\(?[0-9]{3}\)?\s?-?[0-9]{3}-?[0-9]{4}$/;
+
+    if(regphone.test(phone.val())){
+        phone.removeClass("is-invalid");
+        phone.addClass("is-valid");
+    }
+    else{
+        phone.removeClass("is-valid");
+        phone.addClass("is-invalid");
+    }
+
+    let birthday = $('#birthday');
+    let regBday = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+
+    if(regBday.test(birthday.val())){
+        birthday.removeClass("is-invalid");
+        birthday.addClass("is-valid");
+    }
+    else{
+        birthday.removeClass("is-valid");
+        birthday.addClass("is-invalid");
+    }
+}
+
 updateTable();
+
+// There's a button in the form with the ID "addItem"
+// Associate the function showDialogAdd with it.
+let addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+// Button to ave changes from the form
+let saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", saveChanges)
